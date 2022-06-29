fdf02c26d5d51b1f19018f6a2a2183a07a912922
Semyon Sokolov <simionsimion@yandex.ru>
2022-06-29 14:49:22 +0300

    FLAT-7 restructure project for Clean Architecture

---
+++ FlatstackPractice/Scenes/CountryListModule/CountryListRouter.swift
@@ -0,0 +1,8 @@
+//
+//  CountryListRouter.swift
+//  FlatstackPractice
+//
+//  Created by Семён Соколов on 29.06.2022.
+//
+
+import Foundation
---
+++ FlatstackPractice/Services/JsonService.swift
@@ -0,0 +1,8 @@
+//
+//  JsonService.swift
+//  FlatstackPractice
+//
+//  Created by Семён Соколов on 29.06.2022.
+//
+
+import Foundation
