9aca52ee10b0e36332ad30ebbd0a2d68f095d03a
Semyon Sokolov <simionsimion@yandex.ru>
2022-06-29 13:38:03 +0300

    Initial Commit

---
+++ FlatstackPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlatstackPractice
+//
+//  Created by Семён Соколов on 29.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
